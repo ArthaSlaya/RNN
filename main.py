bacf30fc9604928c92d2004af656661991efabb6
ArthaSlaya <siddu.2731995@gmail.com>
2024-12-20 21:16:03 -0500

    Checking the Missingvalue Class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from steps.data_ingestion_step import data_ingestion_step
+from steps.handle_missing_values_step import handle_missing_values_step
 
 if __name__ == "__main__":
     """
@@ -7,3 +8,7 @@ if __name__ == "__main__":
     print("Starting data ingestion...")
     data_ingestion_step()
     print("Data ingestion completed successfully.")
+
+    print("Starting missing values handling...")
+    handle_missing_values_step()
+    print("Missing values handling completed.")
