bfdb4e14b2d267c2980c823882bd3f09d27e5115
ArthaSlaya <siddu.2731995@gmail.com>
2025-01-11 17:36:12 -0500

    Update run_data_ingestion_pipeline.py

--- scripts/run_data_ingestion_pipeline.py
+++ scripts/run_data_ingestion_pipeline.py
@@ -1,9 +1,7 @@
 from pipelines.data_ingestion_pipeline import data_ingestion_pipeline
-from steps.data_ingestion_step import data_ingestion_step
 
 def run_data_ingestion_pipeline() -> None:
     """
     Runs the data ingestion pipeline.
     """
-    pipeline = data_ingestion_pipeline(data_ingestion= data_ingestion_step())
-    pipeline.run()
+    data_ingestion_pipeline()
