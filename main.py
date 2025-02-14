b8f30d1b09e80c1b85a9e9061d95216d928ddc94
ArthaSlaya <siddu.2731995@gmail.com>
2025-02-14 16:33:54 -0500

    added tha vectorization task in the main file

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 from scripts.run_data_ingestion_pipeline import run_data_ingestion_pipeline
 from scripts.run_preprocessing_pipeline import run_preprocessing_pipeline
 from scripts.run_advanced_preprocessing_pipeline import run_advanced_preprocessing_pipeline
+from scripts.run_vectorization_pipeline import run_vectorization_pipeline
 
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(project_root)
@@ -43,11 +44,15 @@ def main():
         print("Starting advanced preprocessing step...")
         run_advanced_preprocessing_pipeline()
 
+    elif args.task == 'vectorize':
+        print('Staring vectorization...')
+        run_vectorization_pipeline()
+
     elif args.task == 'train':
         print("Training task is not implemented yet.")
 
     elif args.task == "deploy":
         print("Deployment task is not implemented yet.")
-
+        
 if __name__ == '__main__':
     main()
